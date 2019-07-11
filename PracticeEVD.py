191e45ee60525c6ab15ef07cccad1b03dfa7699d
edg233 <466670767@qq.com>
2019-07-11 18:46:29 +0800

    Day 1 init

---
+++ PracticeEVD.py
@@ -0,0 +1,21 @@
+'''
+7/11
+Check Tutorial tab to know how to to solve.
+
+Task 
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Input Format
+
+A single line containing a positive integer, .
+
+Constraints 1<=n<=100
+'''
+Code:
+
+
+
